00956ee12b1514d821886b69645e2711fc9fc0c2
shafreeck <shafreeck@gmail.com>
2023-04-01 00:54:12 +0800

    Add missing file / Implement prompt

---
+++ prompt.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/shafreeck/guru/tui"
+)
+
+type AwesomePromptsRepo struct {
+	Url    string
+	Format string
+	Saveas string
+}
+
+var AwesomePromptsRepos = []AwesomePromptsRepo{
+	{Format: "csv", Saveas: "awesome-chatgpt-prompts.csv", Url: "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv"},
+	{Format: "json", Saveas: "awesome-chatgpt-prompts-zh.json", Url: "https://raw.githubusercontent.com/PlexPt/awesome-chatgpt-prompts-zh/main/prompts-zh.json"},
+}
+
+type PromptEntry struct {
+	Act    string
+	Prompt string
+}
+type AwesomePrompts struct {
+	Sess    *session
+	Dir     string
+	Prompts []PromptEntry
+}
+
+func loadCSV(r io.Reader) []PromptEntry {
+	var prompts []PromptEntry
+	reader := csv.NewReader(r)
+	for record, err := reader.Read(); err != io.EOF; record, err = reader.Read() {
+		if err != nil {
+			return nil
+		}
+
+		// ignore invalid lines
+		if len(record) < 2 {
+			continue
+		}
+		// ignore the file header
+		if record[0] == "act" && record[1] == "prompt" {
+			continue
+		}
+
+		prompts = append(prompts, PromptEntry{
+			Act:    record[0],
+			Prompt: record[1],
+		})
+	}
+	return prompts
+}
+func loadJSON(r io.Reader) []PromptEntry {
+	var prompts []PromptEntry
+
+	decoder := json.NewDecoder(r)
+	decoder.Decode(&prompts)
+
+	return prompts
+}
+
+func (ap *AwesomePrompts) sync() error {
+	var prompts []PromptEntry
+	for _, repo := range AwesomePromptsRepos {
+		resp, err := httpClient.Get(repo.Url)
+		if err != nil {
+			return err
+		}
+		r := resp.Body
+		defer r.Close() // there would not have too much repos, so it's ok to use defer
+
+		data, err := io.ReadAll(r)
+		if err != nil {
+			return err
+		}
+		saveas := path.Join(ap.Dir, repo.Saveas)
+		if err := os.WriteFile(saveas, data, 0644); err != nil {
+			return err
+		}
+		fmt.Fprintln(tui.Stdout, blue.Render(repo.Url+" synced"))
+	}
+	ap.Prompts = prompts
+	return nil
+}
+
+func (ap *AwesomePrompts) load() error {
+	prefix := "awesome-chatgpt-prompts"
+	var prompts []PromptEntry
+
+	entries, err := os.ReadDir(ap.Dir)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	var files []string
+	for _, entry := range entries {
+		if strings.HasPrefix(entry.Name(), prefix) {
+			files = append(files, path.Join(ap.Dir, entry.Name()))
+		}
+	}
+	if len(files) == 0 {
+		return nil
+	}
+
+	for _, file := range files {
+		r, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer r.Close()
+		if path.Ext(file) == ".csv" {
+			prompts = append(prompts, loadCSV(r)...)
+		} else if path.Ext(file) == ".json" {
+			prompts = append(prompts, loadJSON(r)...)
+		}
+	}
+	ap.Prompts = prompts
+	return nil
+}
+
+func (ap *AwesomePrompts) actasCommand(ctx context.Context) string {
+	opts := struct {
+		Role []string `cortana:"role, -, -"`
+	}{}
+
+	if len(ap.Prompts) == 0 {
+		fmt.Fprintln(tui.Stderr, red.Render("no prompts found, use ':prompt sync' to sync with the remote repo"))
+		return ""
+	}
+
+	if usage := builtins.Parse(&opts); usage {
+		builtins.Usage()
+		return ""
+	}
+
+	var out, prompt string
+
+	role := strings.Join(opts.Role, " ")
+	for _, p := range ap.Prompts {
+		if p.Act != role {
+			continue
+		}
+		ap.Sess.append(&Message{Role: User, Content: p.Prompt})
+		prompt = p.Prompt
+		out = fmt.Sprintf("***Role***: %s\n\n> %s\n\n", p.Act, tui.WrapWord([]byte(p.Prompt), 80))
+		break // stop when matched
+	}
+	if out == "" {
+		return ""
+	}
+	render := tui.MarkdownRender{}
+	text, _ := render.Render(out)
+	fmt.Fprint(tui.Stdout, string(text))
+	return prompt
+}
+func (ap *AwesomePrompts) listCommand() {
+	render := tui.MarkdownRender{}
+
+	var buf = bytes.NewBuffer(nil)
+	for _, p := range ap.Prompts {
+		out := fmt.Sprintf("***Role***: %s\n\n> %s\n\n", p.Act, tui.WrapWord([]byte(p.Prompt), 80))
+		text, err := render.Render(out)
+		if err != nil {
+			fmt.Fprint(tui.Stderr, red.Render(err.Error()))
+			return
+		}
+		fmt.Fprint(buf, text)
+	}
+	tui.Display[tui.Model[string], string](context.Background(), tui.NewViewport("Awesome ChatGPT Prompts", buf.String()))
+}
+func (ap *AwesomePrompts) syncCommand() {
+	if err := ap.sync(); err != nil {
+		fmt.Fprintln(tui.Stdout, red.Render(err.Error()))
+	}
+	ap.load()
+}
+
+func (ap *AwesomePrompts) RegisterCommands() {
+	builtins.AddCommand(":act as", builtin(ap.actasCommand), "act as a role")
+	builtins.AddCommand(":prompt list", ap.listCommand, "list all prompts")
+	builtins.AddCommand(":prompt sync", ap.syncCommand, "sync prompts with remote repos")
+}
+
+func ActAsComplete(line []rune, pos int) ([][]rune, int) {
+	home, _ := os.UserHomeDir()
+	ap := AwesomePrompts{Dir: path.Join(home, ".guru/prompt")}
+	ap.load()
+
+	prefix := string(line)
+	if prefix == ":act as" {
+		return [][]rune{[]rune(" ")}, 0
+	}
+	// the prefix should has prefix :act as
+	if !strings.HasPrefix(prefix, ":act as ") {
+		return nil, 0
+	}
+
+	n := 50 // return the first n prompts
+	act := strings.TrimPrefix(prefix, ":act as ")
+	var suggests [][]rune
+	for i, p := range ap.Prompts {
+		if strings.HasPrefix(p.Act, act) {
+			if i == n {
+				break
+			}
+			suggests = append(suggests, []rune(strings.TrimPrefix(p.Act, act)))
+		}
+	}
+	return suggests, len(act)
+}
---
+++ tui/viewport.go
@@ -0,0 +1,129 @@
+package tui
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var useHighPerformanceRenderer = false
+
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
+
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return titleStyle.Copy().BorderStyle(b)
+	}()
+)
+
+var _ Model[string] = &Viewport{}
+
+type Viewport struct {
+	title    string
+	content  string
+	ready    bool
+	viewport viewport.Model
+}
+
+func NewViewport(title, content string) *Viewport {
+	return &Viewport{title: title, content: content}
+}
+
+func (m Viewport) Init() tea.Cmd {
+	return nil
+}
+
+func (m Viewport) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+			return m, tea.Quit
+		}
+
+	case tea.WindowSizeMsg:
+		headerHeight := lipgloss.Height(m.headerView())
+		footerHeight := lipgloss.Height(m.footerView())
+		verticalMarginHeight := headerHeight + footerHeight
+
+		if !m.ready {
+			// Since this program is using the full size of the viewport we
+			// need to wait until we've received the window dimensions before
+			// we can initialize the viewport. The initial dimensions come in
+			// quickly, though asynchronously, which is why we wait for them
+			// here.
+			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			m.viewport.YPosition = headerHeight
+			m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
+			m.viewport.SetContent(m.content)
+			m.ready = true
+
+			// This is only necessary for high performance rendering, which in
+			// most cases you won't need.
+			//
+			// Render the viewport one line below the header.
+			m.viewport.YPosition = headerHeight + 1
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height - verticalMarginHeight
+		}
+
+		if useHighPerformanceRenderer {
+			// Render (or re-render) the whole viewport. Necessary both to
+			// initialize the viewport and when the window is resized.
+			//
+			// This is needed for high-performance rendering only.
+			cmds = append(cmds, viewport.Sync(m.viewport))
+		}
+	}
+
+	// Handle keyboard and mouse events in the viewport
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m Viewport) View() string {
+	if !m.ready {
+		return "\n  Initializing..."
+	}
+	return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+}
+func (m Viewport) Value() string {
+	return ""
+}
+func (m Viewport) Error() error {
+	return nil
+}
+func (m Viewport) headerView() string {
+	title := titleStyle.Render(m.title)
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+}
+
+func (m Viewport) footerView() string {
+	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
