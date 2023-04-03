31b24cd6ac0e3e6c7885b32ffc853de8298f6f71
shafreeck <shafreeck@gmail.com>
2023-04-04 02:42:06 +0800

    Add missing file

---
+++ tui/configinput.go
@@ -0,0 +1,154 @@
+package tui
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle.Copy()
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle.Copy()
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Copy().Render("[ Save ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Save"))
+)
+
+var _ Model[[]string] = &ConfigInputModel{}
+
+type ConfigInputModel struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode cursor.Mode
+}
+
+func NewConfigInputModel(prompts ...string) *ConfigInputModel {
+	if len(prompts) == 0 {
+		return nil
+	}
+
+	var inputs []textinput.Model
+	for _, p := range prompts {
+		t := textinput.New()
+		t.Placeholder = p
+		t.CursorStyle = cursorStyle
+		t.CharLimit = 64
+		inputs = append(inputs, t)
+	}
+
+	inputs[0].PromptStyle = focusedStyle
+	inputs[0].TextStyle = focusedStyle
+	inputs[0].Focus()
+
+	return &ConfigInputModel{inputs: inputs}
+}
+
+func (m *ConfigInputModel) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m *ConfigInputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				return m, tea.Quit
+			}
+
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				if i == m.focusIndex {
+					// Set focused state
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+
+			return m, tea.Batch(cmds...)
+		}
+	}
+
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *ConfigInputModel) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m *ConfigInputModel) View() string {
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	b.WriteString(helpStyle.Render("(ctrl+c or esc to quit)"))
+
+	return b.String()
+}
+func (m *ConfigInputModel) Error() error {
+	return nil
+}
+func (m *ConfigInputModel) Value() []string {
+	var vals []string
+	for _, input := range m.inputs {
+		vals = append(vals, input.Value())
+	}
+	return vals
+}
