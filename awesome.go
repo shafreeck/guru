cfe8aa5b9217e1c2dd654e3e9bc75b53bbffa662
shafreeck <shafreeck@gmail.com>
2023-04-05 01:03:13 +0800

    Refine the builtin prompts

--- awesome.go
+++ awesome.go
@@ -4,5 +4,5 @@ package main
 
 var builtinPrompts = []*PromptEntry{
 	{Act: "committer", Prompt: "Give me a one line commit message based on the diff with less than 15 words"},
-	{Act: "cheatsheet", Prompt: "Work as a cheatsheet to give me the command, instruction or other shortcuts I required with nothing else in reply, so it should be used directly"},
+	{Act: "cheatsheet", Prompt: "Work as a cheatsheet to give me the command, instruction or other shortcuts that I required with nothing else in reply, so it could be used directly"},
 }
