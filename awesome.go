e49149652ca1a9dda3744e0f25ad391590cd4406
shafreeck <shafreeck@gmail.com>
2023-04-06 21:55:17 +0800

    Improve the prompt text

--- awesome.go
+++ awesome.go
@@ -3,6 +3,6 @@ package main
 // awesome gives the builtin awesome prompts with guru
 
 var builtinPrompts = []*PromptEntry{
-	{Act: "Committer", Prompt: "Give me a one line commit message based on the diff with less than 10 words"},
+	{Act: "Committer", Prompt: "Give me a one line commit message using the imperative mood based on the diff with less than 10 words"},
 	{Act: "Cheatsheet", Prompt: "Work as a cheatsheet to give me the command, instruction or other shortcuts that I required with nothing else in reply, so it could be used directly"},
 }
