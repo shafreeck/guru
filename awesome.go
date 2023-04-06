40f2718c89a960d0341974daba2fb0b73a4242b8
shafreeck <shafreeck@gmail.com>
2023-04-06 21:50:21 +0800

    Limit committer to 10 words

--- awesome.go
+++ awesome.go
@@ -3,6 +3,6 @@ package main
 // awesome gives the builtin awesome prompts with guru
 
 var builtinPrompts = []*PromptEntry{
-	{Act: "committer", Prompt: "Give me a one line commit message based on the diff with less than 15 words"},
-	{Act: "cheatsheet", Prompt: "Work as a cheatsheet to give me the command, instruction or other shortcuts that I required with nothing else in reply, so it could be used directly"},
+	{Act: "Committer", Prompt: "Give me a one line commit message based on the diff with less than 10 words"},
+	{Act: "Cheatsheet", Prompt: "Work as a cheatsheet to give me the command, instruction or other shortcuts that I required with nothing else in reply, so it could be used directly"},
 }
